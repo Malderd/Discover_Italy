00d9908e53732cce7e971c63e66516a918763533
Malderds <Kosty-k1980@list.ru>
2026-05-06 22:24:08 +0300

    Создание и заполнение файла с валидацией ввода данных пользователем

--- Discover_Italy/active_users.py
+++
@@ -1,5 +0,0 @@
-from bottle import post, request
-import re # Библиотека для паттерна
-from datetime import datetime # Библиотека для даты
-import json
-import os
---
+++ Discover_Italy/files_new_items/validation_activity_user.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+
+def validate_user(nickname, email, birthdate, gender, tour_number):
+    errors = {}
+
+    # Пустые поля
+    if not nickname:
+        errors['nickname'] = "Введите ник"
+
+    if not email:
+        errors['email'] = "Введите email"
+
+    if not birthdate:
+        errors['birthdate'] = "Введите дату"
+
+    if not tour_number:
+        errors['tour_number'] = "Введите номер тура"
+
+    # Почта
+    email_pattern = r'^[\w\.-]+@[\w\.-]+\.\w+$'
+    if email and not re.match(email_pattern, email):
+        errors['email'] = "Некорректный email"
+
+
+    if not birthdate:
+        errors['birthdate'] = "Введите дату"
+    # дата
+    else:
+        try:
+            datetime.strptime(birthdate, "%d-%m-%Y %H:%M")
+        except:
+            errors['birthdate'] = "Формат даты: DD-MM-YYYY HH:MM"
+
+    return errors
