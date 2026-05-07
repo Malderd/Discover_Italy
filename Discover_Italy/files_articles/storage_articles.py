7ae95e769908794b5b3790c65019b2c330a01b84
Dorxyna <danielapagle4@gmail.com>
2026-05-07 18:05:04 +0300

    Создание папки с файлами для статей, создание файла загрузкой и сохранением статей, паттернов для тестирование, и тестирование этих паттеронов

---
+++ Discover_Italy/files_articles/storage_articles.py
@@ -0,0 +1,22 @@
+import json
+import os
+
+# файлы с информацией
+BASE_DIR = os.path.dirname(__file__)
+ARTICLES_FILE = os.path.join(BASE_DIR, 'routes_cities.json')
+
+# загрузка статей
+def load_articles():
+    if not os.path.exists(ARTICLES_FILE):
+        return []
+    with open(ARTICLES_FILE, 'r', encoding='utf-8') as f:
+        try:
+            return json.load(f)
+        except:
+            return []
+        
+
+# сохранение статей
+def save_articles(articles):
+    with open(ARTICLES_FILE, 'w', encoding='utf-8') as f:
+        json.dump(articles, f, ensure_ascii=False, indent=4)
